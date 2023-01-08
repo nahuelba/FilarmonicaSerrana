f6646e8edd5e30c29e51d805189cb2dbeaa66a26
Nahuel Barreiro <nahuelbarreiro@gmail.com>
2023-01-07 22:21:01 -0300

    Opacity animation in mobile menu

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,22 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { link } from '../../interfaces/header.interface';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations:[
+    trigger('menu', [
+      transition(':enter', [
+        style({opacity:0}),
+        animate(300, style({opacity:1})) 
+      ]),
+      transition(':leave', [
+        animate(300, style({opacity:0})) 
+      ])
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
 
